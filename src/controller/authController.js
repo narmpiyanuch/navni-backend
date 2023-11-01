45ec0051b601685469c9bbabc913331380fd2453
money <supalap.ss@gmail.com>
2023-11-01 13:24:00 +0700

    feature getUserProfile refactor login and register fix prisma schema

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,41 +1,69 @@
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const prisma = require("../model/prisma");
-const jwt = require("jsonwebtoken");
 const createError = require("../utils/createError");
-const { loginSchema } = require("../../validators/auth-validator");
-const { registerSchema } = require("../validatetor/authValidate");
+const { registerSchema, loginSchema } = require("../validator/authValidate");
+const createAccessToken = require("../utils/createAccessToken");
 
 exports.login = async (req, res, next) => {
     try {
         const { value, error } = loginSchema.validate(req.body);
-        console.log(value);
         if (error) {
             return next(error);
         }
+        if (value.sub) {
+            const googleUser = await prisma.user.findFirst({
+                where: {
+                    googleId: value.sub,
+                },
+            });
+            if (!googleUser) {
+                const user = await prisma.user.create({
+                    data: {
+                        googleId: value.sub,
+                        email: value.email,
+                    },
+                });
+                const newUser = await prisma.memberInformation.create({
+                    data: {
+                        firstName: value.given_name,
+                        lastName: value.family_name,
+                        userId: user.id,
+                    },
+                });
+
+                await prisma.wallet.create({
+                    data: {
+                        memberInformationId: newUser.id,
+                    },
+                });
+
+                const accessToken = createAccessToken(user.id);
+                return res.status(200).json({ user, accessToken });
+            }
+            if (googleUser) {
+                const accessToken = createAccessToken(googleUser.id);
+                return res.status(200).json({ accessToken, googleUser });
+            }
+        }
+
         const user = await prisma.user.findFirst({
             where: {
                 email: value.email,
-                password: value.password,
             },
         });
-        console.log(user);
         if (!user) {
             return next(createError("Invalid Credential", 400));
         }
-        const matched = await bcrypt.compare(value.password, user.password);
+
+        const matched = bcrypt.compare(value.password, user.password);
         if (!matched) {
             return next(createError("Invalid Credential", 400));
         }
-        const payload = { userId: user.id };
-        const accessToken = jwt.sign(
-            payload,
-            process.env.JWT_SECRET_KEY || "aasdfghjkswjkffkkfkvjnekk",
-            { expiresIn: process.env.JWT_EXPIRE }
-        );
+
+        const accessToken = createAccessToken(user.id);
         delete user.password;
+
         res.status(201).json({
-            message: "LOGIN SUCCESSFULLY",
             accessToken,
             user,
         });
@@ -44,17 +72,13 @@ exports.login = async (req, res, next) => {
     }
 };
 
-exports.getMe = (req, res) => {
-    res.status(200).json({ user: req.user });
-};
-
 exports.register = async (req, res, next) => {
     try {
         const { value, error } = registerSchema.validate(req.body);
         if (error) {
             return next(createError("Invalid Register", 400));
         }
-        const { email, password, role, ...infomation } = value;
+        const { email, password, role, ...information } = value;
 
         const hashPassword = await bcrypt.hash(password, 12);
 
@@ -65,28 +89,33 @@ exports.register = async (req, res, next) => {
                 role,
             },
         });
-
-        infomation.userId = user.id;
-        if (role === "USER") {
-            await prisma.memberInfomation.create({
-                data: infomation,
-            });
-        }
+        information.userId = user.id;
 
         if (role === "ADMIN" || role === "DRIVER") {
             return res.status(400).json({ message: "Invalid access" });
         }
-        const playload = { userId: user.id };
-        const accessToken = jwt.sign(
-            playload,
-            process.env.JWT_SECRET_KEY || "vwe5b32523mrlkqmblkerre",
-            { expiresIn: process.env.JWT_EXPIRE }
-        );
 
+        if (role === "USER") {
+            const newUser = await prisma.memberInformation.create({
+                data: information,
+            });
+
+            await prisma.wallet.create({
+                data: {
+                    memberInformationId: newUser.id,
+                },
+            });
+        }
+
+        const accessToken = createAccessToken(user.id);
         delete user.password;
 
         res.status(200).json({ user, accessToken });
     } catch (error) {
         next(error);
     }
+};
+
+exports.getMe = (req, res) => {
+    res.status(200).json({ user: req.user });
 };
