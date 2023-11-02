18dc49f4b6716b46b9f02eec505647bb15307b11
pakfron <pakpoom.poom@hotmail.com>
2023-11-02 10:11:23 +0700

    add location map

--- src/routes/mapRoute.js
+++ src/routes/mapRoute.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { getDistance } = require('../controller/googleMapController')
+const { getDistance, addWorkArea, addSubArea, selectArea, getSubArea } = require('../controller/googleMapController')
 
 const router = express.Router()
 
 router.get('/distance',getDistance)
-
+router.post('/add',addWorkArea)
+router.post('/add/sub',addSubArea)
+router.post('',selectArea)
+router.get('/get-subarea',getSubArea)
 module.exports= router
