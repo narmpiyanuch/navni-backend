95656813cfedbcadf47195b3fab1522d3094409a
Supaporn Makesirinapapong <supaporn@nestifly.com>
2023-11-05 11:28:31 +0700

    implementing socket.io/backend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
@@ -10,9 +12,26 @@ const errorMiddleware = require("./middleWare/errorMiddleware");
 const notFoundMiddleware = require("./middleWare/notFoundMiddleware");
 const authenticateMiddleware = require("./middleWare/authenticateMiddleware");
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (message) => {
+    io.emit("chat message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("someone has left");
+  });
+});
 
 app.use("/auth", authRoute);
 app.use("/user", authenticateMiddleware, userRoute);
@@ -20,4 +39,4 @@ app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`SERVER RUN IN PORT :${PORT}`));
+httpServer.listen(PORT, () => console.log(`SERVER RUN IN PORT :${PORT}`));
