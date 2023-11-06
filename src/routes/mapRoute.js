c16cb41bfac93982b2e8e2d0cc86d8e2254e6dda
pakfron <pakpoom.poom@hotmail.com>
2023-11-06 10:37:04 +0700

    calculate price

--- src/routes/mapRoute.js
+++ src/routes/mapRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { getDistance, addWorkArea, addSubArea, selectArea, getSubArea } = require('../controller/googleMapController')
+const { getDistance, addWorkArea, addSubArea, selectArea, getSubArea, calculate } = require('../controller/googleMapController')
 
 const router = express.Router()
 
-router.get('/distance',getDistance)
+// router.get('/distance',getDistance)
 router.post('/add',addWorkArea)
 router.post('/add/sub',addSubArea)
 router.post('/select-area',selectArea)
 router.get('/get-subarea',getSubArea)
+router.post('/calculate',calculate)
 module.exports= router
