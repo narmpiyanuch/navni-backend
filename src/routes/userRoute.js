cc65a475cd84a0d5aa25095ea046ee8a83f8d868
money <supalap.ss@gmail.com>
2023-11-06 10:38:38 +0700

    commit after sync

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,20 +1,17 @@
 const express = require("express");
 
 const {
-  getUserProfile,
-  getHistory,
-  transactionIn,
-  transactionOut,
+    getUserProfile,
+    getHistory,
+    transactionIn,
+    transactionOut,
 } = require("../controller/userConroller");
 
 const router = express.Router();
 
 router.get("/", getUserProfile);
-<<<<<<< HEAD
-=======
 router.get("/wallet", getHistory);
 router.post("/transactionIn", transactionIn);
 router.post("/transactionOut", transactionOut);
->>>>>>> f31b77adca1f482e1970652f1b0f3bfc6fe39048
 
 module.exports = router;
