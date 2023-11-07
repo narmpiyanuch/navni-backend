5377921aa0bde0995e9c1efec204ddddfb615d01
money <supalap.ss@gmail.com>
2023-11-07 14:10:11 +0700

    fixed register with google with out family name(lastName)

--- prisma/migrations/20231107070226_migration/migration.sql
+++ prisma/migrations/20231107070226_migration/migration.sql
@@ -14,7 +14,7 @@ CREATE TABLE `user` (
 CREATE TABLE `member_information` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(191) NOT NULL,
-    `last_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NULL,
     `phoneNumber` VARCHAR(10) NULL,
     `stipe_id` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -53,6 +53,24 @@ CREATE TABLE `transaction_out` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `wallet_id` INTEGER NOT NULL,
 
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `RegisterEmployee_Information` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `idCard` VARCHAR(13) NOT NULL,
+    `phone_number` VARCHAR(10) NOT NULL,
+    `gender` ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `RegisterEmployee_Information_idCard_key`(`idCard`),
+    UNIQUE INDEX `RegisterEmployee_Information_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
