a4a6b1bbfa545aea9d18824c53763534be888420
Nitradee8 <nbtt.nik@gmail.com>
2023-11-10 18:10:36 +0700

    feature all driver page in driver controller

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   getAllRegisterDriver,
   createDriver,
+  getAllDriverEmployee,
+  changeDriverStatus
 } = require("../controller/driverController");
 
 const router = express.Router();
 
 router.get("/all-register-driver", getAllRegisterDriver);
 router.post("/create-driver", createDriver);
+router.get("/all-driver-employee",getAllDriverEmployee)
+router.post("/change-status",changeDriverStatus)
 
 module.exports = router;
--- src/routes/driverRoute.js
+++ src/routes/driverRoute.js
@@ -6,18 +6,20 @@ const {
   getAllRegisterDriver,
   getDriverHistory,
   getDriverProfile,
+  getAllDriverEmpolyee
 } = require("../controller/driverController");
 const router = express.Router();
 
 router.post("/register", uploadMiddleware.single("image"), registerDriver);
 
-// router.get(
-//   "/all-register-driver",
-//   authenticateMiddleware,
-//   getAllRegisterDriver
-// );
+router.get(
+  "/all-register-driver",
+  authenticateMiddleware,
+  getAllRegisterDriver
+);
 
 router.get("/profile", authenticateMiddleware, getDriverProfile);
 router.get("/driver-history", authenticateMiddleware, getDriverHistory);
+
 
 module.exports = router;
